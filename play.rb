3c06ba0e38fceb4987101c535b2e36211f6920e3
Arthur <arthurobraqueto@gmail.com>
2023-11-03 12:36:12 +0100

    Hangman-Ruby

--- play.rb
+++ play.rb
@@ -1,77 +1,101 @@
- class Hangman
-   def initialize
-     @word = words.sample
-     @lives = 7
-     @word_teaser = ""
+require_relative 'words'
 
-     @word.first.size.times do
+class Hangman
+  def initialize
+    word_data = WordList.words.sample
+    @secret_word = word_data[:word]
+    @hint = word_data[:hint]
+    @lives = 7
+    @word_teaser = ""
+    @secret_word.size.times do
       @word_teaser += "_ "
     end
-   end
+  end
 
-    def words
-      [
-        ["maison", "Ce qui te sert de toit"],
-      ]
-    end
+  def print_teaser(last_guess = nil)
+    update_teaser(last_guess) unless last_guess.nil?
+    puts @word_teaser
+  end
 
-    def print_teaser last_guess = nil
-      update_teaser(last_guess) unless last_guess.nil?
-      puts @word_teaser
-    end
-
-    def update_teaser last_guess
-      new_teaser = @word_teaser.split
-      new_teaser.each_with_index do |letter, index|
-        if letter == '_' && @word.first[index] == last_guess
-          new_teaser[index] = last_guess
-        end
+  def update_teaser(last_guess)
+    new_teaser = @word_teaser.split
+    new_teaser.each_with_index do |letter, index|
+      if letter == '_' && @secret_word[index] == last_guess
+        new_teaser[index] = last_guess
       end
-
-      @word_teaser = new_teaser.join(' ')
     end
 
-    def make_guess
-      if @lives > 0
-        puts "Choisis une lettre"
-        guess = gets.chomp
+    @word_teaser = new_teaser.join(' ')
+  end
 
-        good_guess = @word.first.include? guess
+  def make_guess
+    while @lives > 0
+      puts "Choisis une lettre"
+      guess = gets.chomp
 
-        if guess == "exit"
-          puts "Merci d'avoir joué !"
-        elsif good_guess
-          puts "Bien joué !"
-
-          print_teaser guess
-
-          if @word.first == @word_teaser.split.join
+      case guess.downcase
+      when "exit"
+        puts "Merci d'avoir joué !"
+        return
+      when "indice"
+        puts "Tu réclames l'aide du tout puissant.. Le voici : #{@hint} \n\n"
+        print_teaser
+      else
+        good_guess = @secret_word.include?(guess)
+        if good_guess
+          puts "Bien joué!"
+          print_teaser(guess)
+          if @secret_word == @word_teaser.split.join
             puts "Félicitations, tu as gagné !"
-          else
-          make_guess
+            return
           end
         else
           @lives -= 1
-          puts "Raté ! Tu as #{ @lives } vies restantes"
-          make_guess
+          puts "Raté ! Il te reste #{@lives} vies."
+          if @lives == 0
+            puts "Game over !"
+          else
+            print_teaser
+          end
         end
-
-      else @lives <= 0
-        puts "Game over !"
       end
     end
+  end
 
-    def begin
-      puts "Bienvenue dans le jeu du pendu... Ton mot a #{ @word.first.size } lettres !"
-      puts "Pour quitter le jeu, tape 'exit'"
+  def begin
+    puts "\n\nBienvenue dans le jeu du pendu...\n\n\n"
+    puts "Tapez 'help' pour avoir les commandes du jeu"
+    puts "Tapez 'start' pour commencer le jeu"
+
+    command_entered = gets.chomp.downcase
+    case command_entered
+    when 'start'
+      puts "C'est parti !"
       print_teaser
-
-      puts "Indice : #{ @word.last }"
-
+      make_guess
+    when 'help'
+      puts "Pour quitter le jeu, tapez 'exit'"
+      puts "Pour avoir un indice, tapez 'indice'"
+      puts "Pour commencer le jeu, tapez 'start'"
+      command_entered = gets.chomp.downcase
+      case command_entered
+      when 'start'
+        puts "C'est parti !"
+        print_teaser
+        make_guess
+      when 'exit'
+        puts "Merci d'avoir joué !"
+      when 'indice'
+        puts "Tu réclames l'aide du tout puissant.. Le voici : #{@hint} \n\n"
+        print_teaser
+        make_guess
+    else
+      print_teaser
       make_guess
     end
-
- end
+    end
+  end
+end
 
 game = Hangman.new
 game.begin
---
+++ words.rb
@@ -0,0 +1,151 @@
+
+module WordList
+  def self.words
+  [
+    {
+      "word": "chat",
+      "hint": "Un animal de compagnie"
+    },
+    {
+      "word": "voiture",
+      "hint": "Un moyen de transport"
+    },
+    {
+      "word": "table",
+      "hint": "Un meuble pour manger"
+    },
+    {
+      "word": "plage",
+      "hint": "Où le sable rencontre la mer"
+    },
+    {
+      "word": "pomme",
+      "hint": "Un fruit commun"
+    },
+    {
+      "word": "ordinateur",
+      "hint": "Une machine électronique"
+    },
+    {
+      "word": "fenetre",
+      "hint": "Pour voir à l'extérieur"
+    },
+    {
+      "word": "television",
+      "hint": "Pour regarder des programmes"
+    },
+    {
+      "word": "oiseau",
+      "hint": "Un animal qui vole"
+    },
+    {
+      "word": "vacances",
+      "hint": "Temps libre pour se détendre"
+    },
+    {
+      "word": "ecole",
+      "hint": "Où l'on apprend"
+    },
+    {
+      "word": "football",
+      "hint": "Un sport populaire"
+    },
+    {
+      "word": "paradoxal",
+      "hint": "Qui contient un paradoxe"
+    },
+    {
+      "word": "ambiguite",
+      "hint": "Manque de clarté dans une expression"
+    },
+    {
+      "word": "equivoque",
+      "hint": "Qui peut être interprété de plusieurs façons"
+    },
+    {
+      "word": "sibyllin",
+      "hint": "Obscur, énigmatique"
+    },
+    {
+      "word": "taciturne",
+      "hint": "Peu bavard, silencieux"
+    },
+    {
+      "word": "delectation",
+      "hint": "Plaisir délicieux, satisfaction"
+    },
+    {
+      "word": "recalcitrant",
+      "hint": "Rétif à l'obéissance"
+    },
+    {
+      "word": "voracite",
+      "hint": "Faim excessive, appétit insatiable"
+    },
+    {
+      "word": "pusillanime",
+      "hint": "Qui manque de courage, timide"
+    },
+    {
+      "word": "ebullition",
+      "hint": "État de liquide qui bout"
+    },
+    {
+      "word": "soporifique",
+      "hint": "Qui provoque le sommeil"
+    },
+    {
+      "word": "ephemere",
+      "hint": "De courte durée, passager"
+    },
+    {
+      "word": "serenite",
+      "hint": "État de calme et de paix intérieure"
+    },
+    {
+      "word": "mysterieux",
+      "hint": "Qui est entouré de mystère, énigmatique"
+    },
+    {
+      "word": "epanouissement",
+      "hint": "Le fait de s'épanouir, de se développer pleinement"
+    },
+    {
+      "word": "incommensurable",
+      "hint": "Qui ne peut pas être mesuré de la même manière"
+    },
+    {
+      "word": "perseverance",
+      "hint": "Action de persévérer, de ne pas abandonner"
+    },
+    {
+      "word": "resilience",
+      "hint": "Capacité à rebondir après des épreuves"
+    },
+    {
+      "word": "equivoque",
+      "hint": "Qui peut être interprété de plusieurs façons"
+    },
+    {
+      "word": "convivialite",
+      "hint": "Qualité de ce qui favorise les relations amicales"
+    },
+    {
+      "word": "inebranlable",
+      "hint": "Qui ne peut pas être ébranlé, solide"
+    },
+    {
+      "word": "sophistique",
+      "hint": "Qui est basé sur des arguments spécieux"
+    },
+    {
+      "word": "idiosyncrasie",
+      "hint": "Caractéristiques personnelles, particularités"
+    },
+    {
+      "word": "insignifiant",
+      "hint": "Qui n'a pas d'importance, sans valeur"
+    }
+  ]
+  end
+end
